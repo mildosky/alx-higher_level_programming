0420c0d53606e774c9fc56c9b42d7a9956fe61a6
mildosky <ibrahimmusah205@gmail.com>
2023-01-03 13:10:23 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print("{:02}".format(i), end=", ")
+for i in range(0, 100):
+    if i == 99:
+        print("99")
     else:
-        print("{:02}".format(i))
+        print("{0:0=2d}, ".format(i), end='')
