3bb5cdd25b97dbed6344a923a1727359669f5d30
mildosky <ibrahimmusah205@gmail.com>
2023-01-05 23:35:15 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
 
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
 if __name__ == "__main__":
-    file = dir(hidden_4)
-    length = len(file)
-    for i in range(0, length):
-        if file[i][0:2] != "__":
-            print(file[i])
+    discovr()
