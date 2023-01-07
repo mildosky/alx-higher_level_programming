8a33aa2058fd1b3c87c696309a733737c6fdca5c
mildosky <ibrahimmusah205@gmail.com>
2023-01-07 22:02:45 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
