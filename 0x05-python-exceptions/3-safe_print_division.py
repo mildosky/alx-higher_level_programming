23cf49a5f93b91bb172672fa1f378fbf01130b48
mildosky <ibrahimmusah205@gmail.com>
2023-01-23 12:37:46 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    response = None
     try:
-        result = a / b
-        print("Inside result: {:.1f}".format(result))
+        response = a / b
     except:
-        result = None
-        print("Inside result: {}".format(result))
+        pass
     finally:
-        return result
+        print("Inside result: {}".format(response))
+    return response
