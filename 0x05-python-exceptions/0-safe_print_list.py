770ceb2fc9b9d58bf4d79e6474b1f66ab2120911
mildosky <ibrahimmusah205@gmail.com>
2023-01-23 12:27:40 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
+    index = 0
+    while True:
         try:
-            print(my_list[index], end='')
-            cmpt += 1
-        except Exception as error:
-            break
-    print('')
-    return cmpt
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
