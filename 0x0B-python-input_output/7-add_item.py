f032104df0869a460086cc271a7e931d1553b3e6
mildosky <ibrahimmusah205@gmail.com>
2023-02-07 12:26:59 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""add item"""
 
-"""
-Saving object from system args module
-"""
+
+import json
 import sys
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-olds = []
-try:
-    olds = load_from_json_file("add_item.json")
-except Exception:
-    pass
-args = sys.argv[1:]
-save_to_json_file(olds+args, "add_item.json")
+
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
