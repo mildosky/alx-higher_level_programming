63479b86694b2b271a4082537b38421b4bde1ba3
mildosky <ibrahimmusah205@gmail.com>
2023-02-07 12:07:55 +0100

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -34,4 +34,4 @@ class Student:
     def reload_from_json(self, json):
         """ Replaces all attributes of the Student instance """
         for atr in json:
-            self.__dict__[atr] = json[atr]
+            self.__dict__[atr] = json[atr] 
