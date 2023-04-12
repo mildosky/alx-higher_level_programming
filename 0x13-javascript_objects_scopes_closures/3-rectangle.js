282eb8df28257c069c8e72fee32b98305e296c40
mildosky <ibrahimmusah205@gmail.com>
2023-04-12 14:57:41 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,24 +3,24 @@
 // Prints a Rectangle with the parameters passed
 
 module.exports = class Rectangle {
-    constructor (width, height) {
-      if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-        this.width = width;
-        this.height = height;
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
+
+      for (; j < this.width; ++j) {
+        process.stdout.write('X');
+      }
+
+      if (j === this.width) {
+        console.log('');
       }
     }
-  
-    print () {
-      for (let i = 0; i < this.height; ++i) {
-        let j = 0;
-  
-        for (; j < this.width; ++j) {
-          process.stdout.write('X');
-        }
-  
-        if (j === this.width) {
-          console.log('');
-        }
-      }
-    }
-  };
+  }
+};
