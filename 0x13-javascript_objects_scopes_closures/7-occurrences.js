163ff6914f1c08b91ffb8d7c6d226a08acdfaaa6
mildosky <ibrahimmusah205@gmail.com>
2023-04-12 15:04:32 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,5 +3,5 @@
 // Returns the number of occurrences of an element in a list
 
 exports.nbOccurences = function (list, searchElement) {
-    return list.filter(x => x === searchElement).length;
-  };
+  return list.filter(x => x === searchElement).length;
+};
