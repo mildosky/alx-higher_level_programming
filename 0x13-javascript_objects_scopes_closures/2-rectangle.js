e6c699e0778adf0f607b582d39b489868ee2c3ab
mildosky <ibrahimmusah205@gmail.com>
2023-04-12 14:58:39 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,10 @@
 // Checks for Rectangle parameters
 
 module.exports = class Rectangle {
-    constructor (width, height) {
-      if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-        this.width = width;
-        this.height = height;
-      }
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
     }
-  };
+  }
+};
