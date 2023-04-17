74b827fbaa61bec4079a2dfbe8549a538fb81632
mildosky <ibrahimmusah205@gmail.com>
2023-04-17 08:29:28 +0100

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 """
-This script prints the first State object
-from the database `hbtn_0e_6_usa`.
+Prints the first State object from the database hbtn_0e_6_usa
 """
-
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-    instance = session.query(State).order_by(State.id).first()
 
-    if instance is None:
-        print('Nothing')
-    else:
-        print('{0}: {1}'.format(instance.id, instance.name))
+    state = session.query(State).order_by(State.id).first()
+
+    print("Nothing" if not state else "{}: {}".format(state.id, state.name))
