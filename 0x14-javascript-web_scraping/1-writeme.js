54f648faefafb3c1af54c21c8d1297a8512f1ddb
mildosky <ibrahimmusah205@gmail.com>
2023-05-23 12:17:16 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
+// A script that writes a string to a file
+
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+const file = process.argv[2];
+const write_ = process.argv[3];
+
+fs.writeFile(file, write_, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
