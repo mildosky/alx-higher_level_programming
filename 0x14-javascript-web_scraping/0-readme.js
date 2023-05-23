0d874e54c0f2abd93ea957f6656274c923c6fc70
mildosky <ibrahimmusah205@gmail.com>
2023-05-23 12:14:59 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
-
-try {
-  const data = fs.readFileSync(file, 'utf-8');
-  console.log(`${data}`);
-} catch (err) {
-  console.log(`${err}`);
-}
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
+  } else {
+    console.log(contents);
+  }
+});
